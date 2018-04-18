54bb4f6ae73ba05ce9791d4111ce4813f13b31a3
ZHaoxin Gu <=>
2018-04-18 12:01:44 -0500

    remove

--- test/oneup_metrics_test.erl
+++ test/oneup_metrics_test.erl
@@ -27,14 +27,14 @@ meter_test_()->
   {timeout, 30, testing_meter()}.
 
 
-oneup_config_test()->
-  oneup_metric_config:init(),
-  oneup_metric_config:insert(test_entry_1,[{atom, test}]),
-  oneup_metric_config:insert(test_entry_2,[{atom, test_0}, {list, [test_1, test_2]}]),
-  oneup_metric_config:start(),
-  undefined = oneup_metric_config:get(test_entry_0, atom),
-  test = oneup_metric_config:get(test_entry_1, atom),
-  [test_1, test_2] = oneup_metric_config:get(test_entry_2, list).
+%%oneup_config_test()->
+%%  oneup_metric_config:init(),
+%%  oneup_metric_config:insert(test_entry_1,[{atom, test}]),
+%%  oneup_metric_config:insert(test_entry_2,[{atom, test_0}, {list, [test_1, test_2]}]),
+%%  oneup_metric_config:start(),
+%%  undefined = oneup_metric_config:get(test_entry_0, atom),
+%%  test = oneup_metric_config:get(test_entry_1, atom),
+%%  [test_1, test_2] = oneup_metric_config:get(test_entry_2, list).
 
 
 
